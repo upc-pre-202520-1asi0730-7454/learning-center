98c89a62ecacafe1892b0fd77bd0b9152fc56aa3
Angel Velasquez <pcisavel@upc.edu.pe>
2025-10-02 16:59:31 -0500

    feat(publishing): add publishing routes and integrate with main router system.

--- src/router.js
+++ src/router.js
@@ -1,19 +1,27 @@
 import Home from "./shared/presentation/views/home.vue";
 import {createRouter, createWebHistory} from "vue-router";
+import publishingRoutes from "./publishing/presentation/publishing-routes.js";
 
 const about = () => import('./shared/presentation/views/about.vue');
 const pageNotFound = () => import('./shared/presentation/views/page-not-found.vue');
 
 const routes = [
-    { path: '/home', name: 'home', component: Home, meta: { title: 'Home' } },
-    { path: '/about', name: 'about', component: about, meta: { title: 'About' } },
-    { path: '/', redirect: '/home' },
-    { path: '/:pathMatch(.*)*', name: 'not-found', component: pageNotFound, meta: { title: 'Page not found' } }
+    { path: '/home',            name: 'home',           component: Home, meta: { title: 'Home' } },
+    { path: '/about',           name: 'about',          component: about, meta: { title: 'About' } },
+    { path: '/publishing',      name: 'publishing',     children: publishingRoutes },
+    { path: '/',                redirect: '/home' },
+    { path: '/:pathMatch(.*)*', name: 'not-found',      component: pageNotFound, meta: { title: 'Page not found' } }
 ];
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
     routes: routes
+});
+
+router.beforeEach((to, from, next) => {
+    let baseTitle = 'ACME Learning Center';
+    document.title = `${to.meta["title"]} | ${baseTitle}`;
+    next();
 });
 
 export default router;
